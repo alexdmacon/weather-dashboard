3219c558d4498b36be3799f79ec41f0c2debebb9
Alex Macon <alexdmacon@gmail.com>
2021-09-12 09:49:19 -0500

    Submit form working and fetching API data.

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ https://getbootstrap.com/docs/4.0/components/forms/
 https://webdesign.tutsplus.com/tutorials/build-a-simple-weather-app-with-vanilla-javascript--cms-33893
 
 https://openweathermap.org/current
+
+https://day.js.org/docs/en/installation/browser
--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,50 @@
-var APIKey = "e256b571cb3fa1813dc8566aad393b43";
 var city;
+var searchForm = document.querySelector("#search-form");
+var searchBtn = document.querySelector("#search-button");
 
 
+function citySearch(event){
+    event.preventDefault();
+    var searchInput = document.querySelector("#city-search-input");
+    city = searchInput.value;
+    if (city) {
+        console.log(city);
+        forecastWeather(city);
+    }
+}
 
+function forecastWeather(city) {
+    var APIKey = "e256b571cb3fa1813dc8566aad393b43";
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    fetch(queryURL)
+    .then(function (response) {
+            response.json().then(function (data) {
+                console.log(data);
+                displayForecast(data, city);
+            }
+            )
+    })
+}
 
-fetch(queryURL);
+searchForm.addEventListener("submit", citySearch);
 
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+/* var searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
+
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+  location.assign(queryString);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit); */ 
