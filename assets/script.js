1ae9aa83eee6bf966dd1302b1f47ea36956e97ef
Alex Macon <alexdmacon@gmail.com>
2021-09-12 16:13:29 -0500

    Displays 5-day forecast.

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ var searchBtn = document.querySelector("#search-button");
 var APIKey = "e256b571cb3fa1813dc8566aad393b43";
 var todayForecast = document.querySelector("#today-forecast");
 var todayDate = dayjs().format("MM/DD/YYYY");
+var fiveDayForecastEls = document.querySelectorAll(".day-forecast")
+var fiveDayDiv = document.querySelector("#five-day-forecast");
 
 // Form submit handler
 searchForm.addEventListener("submit", citySearch);
@@ -18,6 +20,7 @@ function citySearch(event){
     if (city) {
         console.log(city);
         forecastWeather(city);
+        fiveDayForecast(city);
     }
 }
 
@@ -44,7 +47,7 @@ function forecastWeather(city) {
             iconDisplay.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
             todayForecast.append(iconDisplay);
 
-            // display's todays temp
+            // display's today's temp
             var tempDisplay=document.createElement("li");
             tempDisplay.textContent = "Temp: " + data.main.temp + "°F";
             todayForecast.append(tempDisplay);
@@ -62,9 +65,47 @@ function forecastWeather(city) {
             //Where the heck do I find the UV index?
             var uvDisplay=document.createElement("li");
             uvDisplay.textContent = "UV Index: "
-
             }
             )
     })
 }
 
+function fiveDayForecast(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&appid=" + APIKey;
+
+
+    fetch(queryURL)
+    .then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+            
+            var fiveDayDisplay = document.createElement("h2");
+            fiveDayDisplay.textContent="5-Day Forecast";
+            fiveDayDiv.append(fiveDayDisplay);
+
+            // loop populates five-day forecast section with five-day forecast
+            for (let i = 0; i < 5; i++) {
+                fiveDayForecastEls[i].innerHTML="";
+
+                var weatherIcon = data.list[i].weather[0].icon;
+                var iconDisplay = document.createElement("img");
+                iconDisplay.width=100;
+                iconDisplay.height=100;
+                iconDisplay.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+                fiveDayForecastEls[i].append(iconDisplay);
+
+                var tempDisplay=document.createElement("li");
+                tempDisplay.textContent = "Temp: " + data.list[i].main.temp + "°F";
+                fiveDayForecastEls[i].append(tempDisplay);
+
+                var windDisplay=document.createElement("li");
+                windDisplay.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
+                fiveDayForecastEls[i].append(windDisplay);
+
+                var humidityDisplay=document.createElement("li");
+                humidityDisplay.textContent = "Humidity: " + data.list[i].main.humidity + " %";
+                fiveDayForecastEls[i].append(humidityDisplay);
+            }
+        })
+    })
+}
